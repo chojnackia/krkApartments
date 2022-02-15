52578cd7804565530c14771829c1bd1d4cbd94b9
amcc <Adam.Chojnacki@gft.com>
2022-02-15 09:43:18 +0100

    added apartment list and details FE

--- krk-apartments-frontend/src/routes/RouterComponent.tsx
+++ krk-apartments-frontend/src/routes/RouterComponent.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { APARTMENTS_LIST, PAYMENT_CREATE } from "./paths";
+import { APARTMENTS_LIST, APARTMENT_DETAILS, PAYMENT_CREATE } from "./paths";
 import ApartmentList from "../view/apartaments/ApartmentList";
 import PaymentCreate from "../components/PaymentCreate";
+import { ApartmentDetails } from "../components/ApartmentDetails";
 
 
 export default function RouterComponent() {
   return (
     <Routes>
-
       <Route path={PAYMENT_CREATE} element={<PaymentCreate />} />
       <Route path={APARTMENTS_LIST} element={<ApartmentList />} />
+      <Route path={APARTMENT_DETAILS} element={<ApartmentDetails />} />
     </Routes>
   );
 }
